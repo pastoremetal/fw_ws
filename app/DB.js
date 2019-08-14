b97139f79075e986756d0d4f93bc8daf5d162750
pastoremetal <pastoremetal@yahoo.com.br>
2019-08-14 08:59:41 -0300

    update

--- app/DB.js
+++ app/DB.js
@@ -17,29 +17,4 @@ function DB() {
     }
 }
 module.exports = DB;
-/*const Sequelize = require('sequelize');
-function DB() {
-    let host = "localhost";
-    let user = "root";
-    let pass = "1478963";
-    let database = "falling_worlds";
-    let con = null;
-    let connect = function () {
-        con = new Sequelize(database, user, pass, {
-            host: host,
-            dialect: 'mysql',
-            operatorsAliases: false,
-        }).authenticate()
-            .then(() => { console.log('Connection has been established successfully.') })
-            .catch(err => {
-                throw err;
-                console.error('Unable to connect to the database:', err);
-            });
-    };
-    this.getConn = function () {
-        if (con === null)
-            connect();
-        return con;
-    }
-}
-module.exports = DB;*/
+
